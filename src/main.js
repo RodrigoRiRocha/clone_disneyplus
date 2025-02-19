d81f8df321f99690d9c085e1c99226a0b10aa79c
Rodrigo <rstuteu@gmail.com>
2025-02-19 05:57:24 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Seleciona todos os botões com o atributo data-tab-button
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
     // Adiciona um ouvinte de evento de clique a cada botão
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
@@ -19,21 +33,43 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is--active');
         });
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 });
 
 // Função para remover a classe ativa de todos os botões
-function removeBotaoAtivo () {
+function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is--active');
     }
 }
 
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is--open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
 // Função para esconder todas as abas
-function escondeTodasAbas () {
+function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]'); // Seleciona todos os elementos com data-tab-id
 
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is--active');
     }
+}
+
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
